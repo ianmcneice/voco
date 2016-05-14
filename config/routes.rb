60d357e4114f23a9b5444ac79157b203940198bf
ianmcneice <ian.mcneice@flatbook.co>
2016-05-14 00:29:58 -0400

    Made template for landing page
    - added note to readme that I can’t figure out how to load image assets
    when page isn’t root
    - switched to the temp landing page as root
    - took away irrelevant sections to work off of

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'temp/home'
+  get 'temp/home' => 'temp#home'
 
-  get 'welcome/homepage'
+  get 'welcome/homepage' => 'welcome#homepage'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
