2642d69df91841aed302a1e3d15838333fad65be
ianmcneice <ian.mcneice@flatbook.co>
2016-05-13 23:32:21 -0400

    Added temp landing page
    - added temp landing controller
    - moved styles from homepage.html.erb to application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'temp/home'
+
   get 'welcome/homepage'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#homepage'
+  root 'temp#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
